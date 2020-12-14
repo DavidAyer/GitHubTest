10167a63b3aa3d59103ae58245852a80519b71d0
David Ayer <davidayer.22@gmail.com>
2020-12-14 12:38:42 -0500

    Added clarification 5

--- Doc/Doc1.py
+++ Doc/Doc1.py
@@ -1,6 +1,6 @@
 '''
 sdckmdlkwdmfekvmfeve;lv,;lfv,l;f
 kcmdcmdcvmdfvmdfvbmrgf;bmfglbmfvldl
-dckdmcksdmcsdmcldscldcldc,ldsc,l
+dckdmcksdmcsdmcldscldjnjnjnjnjcldc,ldsc,l
 c,w'cdc;d.c;.c;c.;d.c;c.
 '''
